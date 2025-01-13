e3cc3b6466c7573b95b4691cf61b665aff344fdf
unknown <rijal.1344@gmail.com>
2025-01-13 07:21:57 +0700

    feat: add store user_role after store user

--- src/service/register-service.ts
+++ src/service/register-service.ts
@@ -1,16 +1,18 @@
 import { prisma } from "../app/database";
 import { ErrorResponse } from "../error/error-response";
 import {
-  toRegisterUserResponse,
-  type RegisterRequest,
-  type RegisterResponse,
+  toRegisterStudentUserResponse,
+  type RegisterStudentRequest,
+  type RegisterStudentResponse,
 } from "../model/register-model";
 import { RegisterValidation } from "../validation/register-validation";
 import { Validation } from "../validation/validation";
 
 export class RegisterService {
-  static async storeUser(request: RegisterRequest): Promise<RegisterResponse> {
-    const requestBody: RegisterRequest = Validation.validate(
+  static async storeUserStudent(
+    request: RegisterStudentRequest
+  ): Promise<RegisterStudentResponse> {
+    const requestBody: RegisterStudentRequest = Validation.validate(
       RegisterValidation.registerSchema,
       request
     );
@@ -35,18 +37,33 @@ export class RegisterService {
       throw new ErrorResponse(404, "nim already exist");
     }
 
+    const role = await prisma.role.findUnique({
+      where: {
+        name: "student",
+      },
+    });
+
+    if (!role) {
+      throw new ErrorResponse(404, "role not found");
+    }
+
     const hashedPassword = await Bun.password.hash(requestBody.password);
-    const [storedUser] = await prisma.$transaction([
+    const [storedUserStudent] = await prisma.$transaction([
       prisma.user.create({
         data: {
           name: requestBody.name,
           nim: requestBody.nim,
           email: requestBody.email,
           password: hashedPassword,
+          user_role: {
+            create: {
+              role_id: role.id,
+            },
+          },
         },
       }),
     ]);
 
-    return toRegisterUserResponse(storedUser);
+    return toRegisterStudentUserResponse(storedUserStudent);
   }
 }

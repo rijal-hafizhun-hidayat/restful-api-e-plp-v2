6a4a3a9b99d30669e3dc6cf9460461b2a374bc75
unknown <rijal.1344@gmail.com>
2025-01-13 07:06:21 +0700

    seed: add role seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const roles = await prisma.role.createMany({
+    data: [
+      {
+        name: "admin",
+      },
+      {
+        name: "mahasiswa",
+      },
+      {
+        name: 'dpl'
+      }
+    ],
+  });
+
+  console.log(roles);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
